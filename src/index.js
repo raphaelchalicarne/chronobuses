62cc35e34ef17600e732ef1d84f923263336ecf3
Raphaël Chalicarne <raphael.chalicarne@foodyssey.co>
2024-05-27 11:17:59 +0200

    :sparkles: Script + Style

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicmFwaDU5IiwiYSI6ImNsYWY3bHRxcTA4dTEzeG83ZjB2bzkwYmUifQ.Ye2p9OAgykrdKe0fLWSN7A';
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/light-v10',
-  center: [4.834277, 45.763420], // starting position
-  zoom: 5, // starting zoom
-  attributionControl: true,
-  customAttribution: ['<b><a href="https://github.com/raphaelchalicarne/chronobuses">GitHub</a></b>'],
-});
+var map = L.map('map').setView([45.7578137, 4.8320114], 8);
 
-// automatically resize map to always match the window's size
-const el = document.getElementById('map')
-const resize = () => {
-  const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-  const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-  el.style.width = w + 'px'
-  el.style.height = h + 'px'
-  map.resize()
-}
-resize()
-window.addEventListener('resize', resize)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
