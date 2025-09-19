1b8cd71f092ceb413a84780d200c1bfc7c05560e
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-09-19 16:32:48 +0200

    :see_no_evil: Ignore external scripts errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores(["assets/"]),
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
